ad03c4dcde809d22caeb46dd0811dfe842b2368d
rajecc <66696191+rajecc@users.noreply.github.com>
2022-10-23 20:30:05 +0300

    Add files via upload

---
+++ app/scripts/mysite.js
@@ -0,0 +1,40 @@
+var h3Showed = false;
+
+function changeDisplay() {
+	const header3 = document.getElementById(1);
+	header3.style.display=h3Showed ? "block" : "none";
+	h3Showed = !h3Showed;
+
+}
+function changeDisplay1() {
+	const header3 = document.getElementById(3);
+	header3.style.display=h3Showed ? "block" : "none";
+	h3Showed = !h3Showed;
+
+}
+function changeDisplay2() {
+	const header3 = document.getElementById(2);
+	header3.style.display=h3Showed ? "block" : "none";
+	h3Showed = !h3Showed;
+
+}
+
+function changeDisplay3() {
+	const header3 = document.getElementById(4);
+	header3.style.display=h3Showed ? "block" : "none";
+	h3Showed = !h3Showed;
+}
+
+function changeDisplay4() {
+	const header3 = document.getElementById(5);
+	header3.style.display=h3Showed ? "block" : "none";
+	h3Showed = !h3Showed;
+}
+
+function changeDisplay5() {
+	const header3 = document.getElementById(6);
+	header3.style.display=h3Showed ? "block" : "none";
+	h3Showed = !h3Showed;
+}
+
+
